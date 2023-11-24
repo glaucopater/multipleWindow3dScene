bdb257f7cfbced712a3f927476cccf29a42e0557
“Glauco@Greenphoenix” <glauco.pater@gmail.com>
2023-11-24 17:56:01 +0100

    Setup Project

---
+++ index.js
@@ -0,0 +1,13 @@
+import './style.css'
+import * as module from './src/main.js'
+
+document.querySelector('#app').innerHTML = `
+<style type="text/css">
+    *
+    {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+    <div id="app"></div>
+`
